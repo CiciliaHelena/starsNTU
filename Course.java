da4675aaafbf28987b989bda0f1cfa1e1f30f111
genisonwibawa <44596990+genisonwibawa@users.noreply.github.com>
2018-11-15 22:24:07 +0800

    delete boolean rese

--- Course.java
+++ Course.java
@@ -15,7 +15,7 @@ public class Course implements Serializable
 	private int initialVacancy;
 	private int examWeightage;
 	private Map <String, Integer> courseworkComponent = new Hashtable<String, Integer>();  // <type, weightage>
-	boolean
+
 
 
 
